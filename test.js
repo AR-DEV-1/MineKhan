7cdb3b39229847ca9227456ecac8f3512cb13fd8
FlameV3 <FlameX.D@skiff.com>
2023-10-07 06:44:13 -0400

    Create test.js
    Signed-off-by: FlameV3 <FlameX.D@skiff.com>

---
+++ test.js
@@ -0,0 +1,11 @@
+// This is a JavaScript code snippet
+function openLink() {
+  // Define the URL you want to open
+  const linkUrl = 'https://example.com';
+
+  // Open the link in a new tab
+  window.open(linkUrl, '_blank');
+}
+
+// Call the function to open the link when needed
+openLink();
