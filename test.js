26c43381dfaf6af4b6f4efc05c77f59c2affa52a
FlameV3 <FlameX.D@skiff.com>
2023-10-07 06:45:27 -0400

    Update test.js
    Signed-off-by: FlameV3 <FlameX.D@skiff.com>

--- test.js
+++ test.js
@@ -1,7 +1,8 @@
+// Made By FlamePVPCOdes inside his penis
 // This is a JavaScript code snippet
 function openLink() {
   // Define the URL you want to open
-  const linkUrl = 'https://example.com';
+  const linkUrl = 'https://www.khanacademy.org/computer-programming/minekhan/5647155001376768';
 
   // Open the link in a new tab
   window.open(linkUrl, '_blank');
