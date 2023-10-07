90dc534f50c472c82883ef424f24fd50e76114c0
FlameV3 <FlameX.D@skiff.com>
2023-10-07 06:36:17 -0400

    Update webpack.config.js
    Signed-off-by: FlameV3 <FlameX.D@skiff.com>

--- webpack.config.js
+++ webpack.config.js
@@ -1,57 +1,65 @@
-const HtmlWebpackPlugin = require("html-webpack-plugin")
-const InlinePlugin = require("html-inline-script-webpack-plugin")
-const path = require("path")
-const fs = require('fs')
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+const fs = require("fs");
 
-let plugins = [
-	new HtmlWebpackPlugin({
-		template: './src/index.html',
-		inject: "body",
-	})
-]
+const isDevelopment = process.env.NODE_ENV === "development";
 
-// Build to the dist folder by default
-let output = "./dist"
+const plugins = [
+  new HtmlWebpackPlugin({
+    template: "./src/index.html",
+    inject: "body",
+  }),
+];
 
-// If I'm coding on my VPS, build to the beta page on willard.fun
-if (__dirname.includes("willard/server/dev")) {
-	output = "../../public/minekhan/beta"
+let outputPath = "./dist";
+
+if (isDevelopment && __dirname.includes("willard/server/dev")) {
+  outputPath = "../../public/minekhan/beta";
+} else if (
+  isDevelopment &&
+  __dirname.includes("/home/willard/Desktop/MineKhan")
+) {
+  const vpsPath = "/home/willard/vps";
+  if (fs.existsSync(vpsPath) && fs.readdirSync(vpsPath).includes("server")) {
+    outputPath = path.join(vpsPath, "server", "public", "minekhan", "beta");
+  }
 }
-
-// If I'm coding on my desktop, and have my VPS mounted, build to the beta page on willard.fun
-else if (__dirname.includes("/home/willard/Desktop/MineKhan")) {
-	if (fs.readdirSync("/home/willard/vps").includes("server")) {
-		output = "/home/willard/vps/server/public/minekhan/beta"
-	}
-}
-
-// Bundle the JS into the HTML file; makes debugging harder, and breaks webpack's auto build
-// plugins.push(new InlinePlugin())
 
 module.exports = {
-	mode: "none",
-	entry: "./src/main.js",
-	output: {
-		path: path.resolve(__dirname, output),
-		filename: "main.js",
-	},
-	module: {
-		rules: [
-			{
-				test: /\.css$/i,
-				use: ["style-loader", "css-loader"],
-			},
-			{
-				test: /\.(txt|glsl)$|workers\//i,
-				use: "raw-loader",
-			},
-		],
-	},
-	plugins,
-	watch: true,
-	watchOptions: {
-		aggregateTimeout: 100,
-		// poll: 1000,
-		ignored: '**/node_modules',
-	},
-}
+  mode: isDevelopment ? "development" : "production",
+  entry: "./src/main.js",
+  output: {
+    path: path.resolve(__dirname, outputPath),
+    filename: "main.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(txt|glsl)$|workers\//i,
+        use: "raw-loader",
+      },
+    ],
+  },
+  plugins,
+  watch: isDevelopment,
+  watchOptions: {
+    aggregateTimeout: 100,
+    ignored: "**/node_modules",
+  },
+  devtool: isDevelopment ? "eval-source-map" : false,
+  optimization: {
+    minimize: !isDevelopment,
+  },
+  performance: {
+    hints: isDevelopment ? "warning" : "error",
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, outputPath),
+    compress: true,
+    port: 9000,
+  },
+};
